3dbda3b7ef56e7af6d3e2f665fa8d9891f05fd56
Aborisade <bobleeswaggs123@gmail.com>
2024-04-02 23:02:42 +0200

    gotta go read now

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import React from 'react'
+import Contact from './Components/Contact' 
+import LoginForm from './Components/LoginForm' 
+import Home from './Components/Home'
+import Privacy from './Components/Privacy'
+import SignUp from './Components/SignUp'
+import {Route, Routes} from "react-router-dom"
+import Navbar from './Components/Navbar'
 
 const App = () => {
   return (
-    <div>App, Let's write some code iy's been a wjile., i womsjd uyiud</div>
+    <div>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='login' element={<LoginForm/>} />
+        <Route path='privacy' element={<Privacy/>} />
+        <Route path='signup' element={<SignUp/>} />
+        <Route path='contact' element={<Contact/>} />
+      </Routes>
+    </div>
   )
 }
 
